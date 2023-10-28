f9110a65d3b17f97d97461c5b294620a79f96336
krzysiou <krzysztof.kt.tluszcz@gmail.com>
2023-10-28 18:24:50 +0200

    feat: check for fields when updating file

--- src/core/file-actions/update-file.ts
+++ src/core/file-actions/update-file.ts
@@ -7,6 +7,38 @@ const updateFile = async (req: Request, res: Response) => {
   const requestingUser: User = req.body.user;
   const requestingFile: File = req.body.file;
 
+  const { title, form } = requestingFile;
+  const { info, mainSubjects } = form;
+  const {
+    name,
+    surname,
+    albumNumber,
+    fieldOfStudy,
+    email,
+    level,
+    term,
+    year,
+    dean,
+  } = info;
+
+  if (
+    !title ||
+    !name ||
+    !surname ||
+    !albumNumber ||
+    !fieldOfStudy ||
+    !email ||
+    !level ||
+    !term ||
+    !year ||
+    !dean ||
+    mainSubjects.length === 0
+  ) {
+    return res
+      .status(400)
+      .send({ message: 'You need to provide all information' });
+  }
+
   const user = findUser({ id: requestingUser.id });
 
   if (!user) return res.status(404).send({ message: 'User not found' });
