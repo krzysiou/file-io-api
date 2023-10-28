3a37cf4a4016a244b8144fd5cce9b85f9b64536a
krzysiou <krzysztof.kt.tluszcz@gmail.com>
2023-10-28 19:43:01 +0200

    refactor: change code formatting

--- src/core/fetch-actions/get-file-data.ts
+++ src/core/fetch-actions/get-file-data.ts
@@ -8,11 +8,9 @@ const getFileData = async (req: Request, res: Response) => {
   const id = req.body.id;
 
   const user = findUser({ id: requestingUser.id });
-
   if (!user) return res.status(404).send({ message: 'User not found' });
 
   const file = user.files.find((file) => file.id === id);
-
   if (!file) return res.status(404).send({ message: 'File not found' });
 
   return res.status(200).send(file);
